99e3bfed662c56f0f7f8600ab3b27dcb576a896a
goroya <goroyahoo@gmail.com>
2017-06-28 07:26:22 +0900

    add package

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import logo from './logo.svg';
 import './App.css';
 
+injectTapEventPlugin();
+
 class App extends Component {
   render() {
-    return (
+    return (<MuiThemeProvider>
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-header"><img src={logo} className="App-logo" alt="logo"/>
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p></div>
+    </MuiThemeProvider>);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+/*
+const store = createStore(
+    reducer,
+    applyMiddleware(logger)
+);
+*/
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
